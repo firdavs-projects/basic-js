1fb98f53e325e913062a14dc3f121d544dbb3be8
Firdavs Abdulloev <firdavs.abdulloev.7725@gmail.com>
2022-04-30 13:34:27 +0500

    feat: resolve tests

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight( arr) {
+  return arr.sort((a, b) => {
+    if (a === -1) {
+      return a;
+    }
+    if (b === -1) {
+      return b;
+    }
+    return a - b;
+  });
+  // throw new NotImplementedError();
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -3,19 +3,43 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform( arr ) {
+  if ( !Array.isArray(arr) ) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  return arr.map( (item, index) => {
+    if (item === '--double-next') {
+      if(arr[index + 1]) {
+        return arr[index + 1];
+      } else {
+        return undefined;
+      }
+    } else if (item === '--double-prev') {
+      if (arr[index - 1]) {
+        return arr[index - 1];
+      } else {
+        return undefined;
+      }
+    } else if (item === '--discard-prev') {
+      arr[index - 1] = undefined;
+      return undefined;
+    } else if (item === '--discard-next') {
+      arr[index + 1] = undefined;
+      return undefined;
+    }  else {
+      return item;
+    }
+  }).filter( item => item !== undefined );
 }
 
 module.exports = {
